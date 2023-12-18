b0011855c03a3e29797dcb946671f29a8affddd1
Sebastien Gallet <sgallet.dev@gmail.com>
2023-12-18 16:47:05 +0100

    add comment at pikadayPicker

--- src/components/pikadayPicker.jsx
+++ src/components/pikadayPicker.jsx
@@ -3,19 +3,22 @@ import Pikaday from 'pikaday';
 import 'pikaday/css/pikaday.css';
 
 
+// Définition du composant PikadayPicker
 const PikadayPicker = ({ value, onChange }) => {
   const ref = useRef(null);
 
   useEffect(() => {
+    // Création d'une instance de Pikaday avec les options spécifiées
     const picker = new Pikaday({
-      field: ref.current,
-      onSelect: onChange,
+      field: ref.current, // Utilisation de la référence pour cibler l'élément input
+      onSelect: onChange, // Appel de la fonction onChange lorsqu'une date est sélectionnée
     });
+
+    // Destruction de l'instance de Pikaday lors du démontage du composant
     return () => picker.destroy();
   }, [onChange]);
 
-
+  // Rendu du composant avec un champ de texte en lecture seule
   return <input type="text" ref={ref} value={value} readOnly />;
 };
-
 export default PikadayPicker;
